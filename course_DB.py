c2338ce8967d7a10313eeeb17bad701905bb2c05
Egor Siropov <stafffsss@mail.ru>
2020-07-25 12:09:00 +0300

    ver 1

---
+++ course_DB.py
@@ -0,0 +1,90 @@
+import psycopg2 as pg
+
+def create_db():
+    """Создание таблиц"""
+
+    #таблица студента
+    cur.execute('''
+        create table if not exists Student(student_id serial primary key,
+        name varchar(100) not null,
+        gpa numeric(10,2) null,
+        birth timestamp with time zone null);''')
+
+    #таблица курса
+    cur.execute('''
+        create table if not exists Course(
+        course_id integer primary key,
+        name varchar(100) not null
+        );
+        ''')
+
+    #связующая таблица
+    cur.execute('''
+        create table if not exists intermediate_Course_Stud(
+        id_course integer references  Course(course_id),
+        id_stud integer references  Student(student_id)
+        
+        );
+        ''')
+def add_students(course_id, students):
+    """создает студентов и записывает их на курс"""
+    for man in students:
+        cur.execute('''insert into Student(name, gpa, birth) values(%s, %s, %s) RETURNING student_id;''',
+                    (man['name'], man['gpa'], man['birth']))
+        man_id = cur.fetchall()[0][0]
+        cur.execute('''insert into intermediate_Course_Stud(id_course, id_stud) values(%s, %s);''',
+        (course_id, man_id))
+
+def get_students(course_id):
+    """возвращает студентов определенного курса"""
+    cur.execute('select * from intermediate_Course_Stud where id_course=(%s);', (course_id, ))
+    data = (cur.fetchall())
+    for item in data:
+        id_stud = item[1]
+        get_student(id_stud)
+
+
+def add_student(student):
+    """Создает студента"""
+    cur.execute('''
+                insert into Student(name, gpa, birth) values(%s, %s, %s);''',
+                (student['name'], student['gpa'], student['birth']))
+
+
+def get_student(student_id):
+    """Возвращает данные студента"""
+    cur.execute('select * from Student where student_id=(%s);', (student_id,))
+    print(cur.fetchall())
+    data = cur.fetchall()
+    return data
+
+
+def create_course(course_id, name):
+    """Создает курс"""
+    cur.execute('''insert into Course(course_id, name) values(%s, %s);''', (course_id, name))
+
+
+if __name__ == '__main__':
+    with pg.connect(database='neto_hw', user='neto',
+                    password='1234', host='localhost', port=5432) as conn:
+        cur = conn.cursor()
+    create_db()
+    create_course(1, 'Python')
+    create_course(2, 'Java')
+
+    students = [
+        {'name': 'Vasia', 'gpa': '5.0', 'birth': '1870-12-16'},
+        {'name': 'Gena', 'gpa': '1.3', 'birth': '2021-11-16'},
+        {'name': 'Lena', 'gpa': '4.0', 'birth': '1992-01-13'}
+    ]
+
+    students_1 = [
+        {'name': 'Vasia', 'gpa': '5.0', 'birth': '1870-12-16'},
+        {'name': 'Gena', 'gpa': '1.3', 'birth': '2021-11-16'},
+        {'name': 'Lena', 'gpa': '4.0', 'birth': '1992-01-13'}
+    ]
+
+    student = {'name': 'Lena', 'gpa': '4.0', 'birth': '1992-01-13'}
+    add_students(1, students)
+    add_students(2, students_1)
+    get_students(2)
